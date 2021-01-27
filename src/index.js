bda86cd3f7ab2c70801018b67755024cd4b59c50
a4anthony <55565463+a4anthony@users.noreply.github.com>
2021-01-27 20:31:50 +0100

    stable point

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+import Vue from "vue";
 import Lander1 from "@/components/Landers/Lander1";
 import TestButton from "@/components/Buttons/TestButton";
 
-export { Lander1, TestButton };
+const Components = {
+  Lander1,
+  TestButton
+};
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name]);
+});
+
+export default Components;
